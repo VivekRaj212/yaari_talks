6586f3892f715f4cf649389491b842551b429f7d
Vivek <rajbharvivek513@gmail.com>
2026-01-06 14:01:24 +0530

    Fix Express type augmentation

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -2,8 +2,14 @@ import { UserDocument } from "../models/user.model";
 
 declare global {
   namespace Express {
+    interface Request {
+      user?: UserDocument;
+    }
+    
     interface User extends UserDocument {
       _id?: any;
     }
   }
 }
+
+export {};
