a3e95ae998839e3e4b25a8a11e001f575e7625ba
Vivek <rajbharvivek513@gmail.com>
2026-01-06 14:21:31 +0530

    Upgrade Express to v5

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,15 +1,9 @@
-// import { UserDocument } from "../models/user.model";
+import { UserDocument } from "../models/user.model";
 
-// declare global {
-//   namespace Express {
-//     interface User extends UserDocument {
-//       _id?: any;
-//     }
-//   }
-// }
-
-declare namespace Express {
-  interface Request {
-    user?: import("../models/user.model").UserDocument;
+declare global {
+  namespace Express {
+    interface User extends UserDocument {
+      _id?: any;
+    }
   }
 }
